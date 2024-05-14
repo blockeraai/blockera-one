639a818e5f0a6ecf02a1778f975af216a9de301c
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-14 17:02:23 +0330

    feat: "Sidebar" template part registration

--- packages/blockera-one/php/Providers/AppServiceProvider.php
+++ packages/blockera-one/php/Providers/AppServiceProvider.php
@@ -22,10 +22,16 @@ class AppServiceProvider extends ServiceProvider {
 		add_action( 'init', [ $this, 'loadTextDomain' ] );
 
 		add_action( 'after_setup_theme', [ $this, 'after_setup_theme' ] );
+
+		add_filter( 'default_wp_template_part_areas',  [ $this, 'template_part_areas' ] );
 	}
 
 	/**
 	 * After theme setup
+	 * 
+	 * @hooked `after_setup_theme`
+	 * 
+	 * @return void
 	 */
 	public function after_setup_theme(): void {
 
@@ -49,4 +55,25 @@ class AppServiceProvider extends ServiceProvider {
 		load_plugin_textdomain( 'blockera-one', false, BLOCKERA_ONE_PATH . '/languages' );
 	}
 
+	/**
+	 * Add a Sidebar template part area
+	 * 
+	 * @hooked `default_wp_template_part_areas`
+	 * 
+	 * @param  array $areas Template part areas list
+	 * 
+	 * @return array
+	 */
+	function template_part_areas( array $areas ) {
+
+		$areas[] = array(
+			'area'        => 'sidebar',
+			'area_tag'    => 'section',
+			'label'       => __( 'Sidebar', 'blockera-one' ),
+			'description' => __( 'The Sidebar template specifies a page area that is available in the Page (With Sidebar) template.', 'blockera-one' ),
+			'icon'        => 'sidebar',
+		);
+
+		return $areas;
+	}
 }
