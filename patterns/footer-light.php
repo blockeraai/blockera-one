edf434b88a7fc5080f593750691b74805ef4ff62
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-14 17:26:09 +0330

    feat: add footer and header temp patterns

---
+++ patterns/footer-light.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Footer Light
+ * Slug: footer-light
+ * Description:
+ * Categories: footer
+ * Keywords:
+ * Viewport Width: 1500
+ * Block Types: core/template-part/footer
+ * Post Types:
+ * Inserter: true
+ */
+?>
+
+TODO: add footer pattern template
---
+++ patterns/header-light.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Header Light
+ * Slug: header-light
+ * Description: Header with nav and social icons
+ * Categories: header
+ * Keywords: header, nav, links, button
+ * Viewport Width: 1500
+ * Block Types: core/template-part/header
+ * Inserter: true
+ */
+?>
+
+TODO: add footer pattern template
