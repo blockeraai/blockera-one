0ca5587933641bc6da1b1cf5d5a8c79a18e2d439
rezaelahidev <rezaelahi.dev@gmail.com>
2026-08-11 20:47:48 +0330

    chore: customized .eslintignore pattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 const base = require('./packages/global-packages/packages/dev-tools/js/eslint/config');
 
+const ignorePatterns = base.ignorePatterns.concat(['/bin/**']);
+
 module.exports = {
 	...base,
+	ignorePatterns,
 	rules: {
 		...base.rules,
 		'@wordpress/i18n-text-domain': [
