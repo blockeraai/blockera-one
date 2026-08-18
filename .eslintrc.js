3d33f2f38604360321dc46f4497cac4c730c9a01
Ali Aghdam <betteraliaghdam@gmail.com>
2026-08-18 20:43:33 +0330

    chore: sync project configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+// Edit packages/global-packages/packages/dev-tools/root-configs/.eslintrc.blockera-one.js
+// project:bootstrap copies this to the host repo root for --project=blockera-one.
 const base = require('./packages/global-packages/packages/dev-tools/js/eslint/config');
 
 const ignorePatterns = base.ignorePatterns.concat(['/bin/**']);
