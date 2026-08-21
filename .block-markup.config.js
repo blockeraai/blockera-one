9dff43bd2fb411076ccf19bdcc7324c63dc2d5ee
Ali Aghdam <betteraliaghdam@gmail.com>
2026-08-21 17:53:11 +0330

    feat: wire block-markup normalize for theme patterns and templates

---
+++ .block-markup.config.js
@@ -0,0 +1,17 @@
+/**
+ * Block-markup pipeline for Blockera One.
+ *
+ * Merged onto the GP base at
+ * `packages/global-packages/packages/dev-tools/js/block-markup/base-config.js`.
+ * Consumed by:
+ * - `npm run block-markup:normalize` / `block-markup:check` / `block-markup:prettier`
+ * - NormalizeBlockMarkupWebpackPlugin during `npm run start`
+ */
+
+module.exports = {
+	textDomain: 'blockera-one',
+	uriPhpExpression: 'get_template_directory_uri()',
+	imagePathRoots: ['assets', 'patterns/images'],
+	patternsDirs: ['patterns', 'patterns-woocommerce'],
+	templatesDirs: ['templates'],
+};
